7720600e7e20d52335a0088fe32e5713d112a792
Prem  Chowdhry <prem.chowdhry17@imperial.ac.uk>
2021-02-02 13:36:30 +0000

    Added query response to what is your name with test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,14 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("wordsworth")) {
+            return "William Wordsworth (7 April 1770 – 23 April 1850) was an English " +
+                    "Romantic poet who, with Samuel Taylor Coleridge, helped to launch the " +
+                    "Romantic Age in English literature with their joint publication Lyrical Ballads (1798)";
+        }
+        if (query.toLowerCase().contains("what is your name")) {
+            return "PRS";
+        }
         return "";
     }
 }
